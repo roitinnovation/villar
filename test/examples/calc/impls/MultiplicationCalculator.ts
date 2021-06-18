ed9edcc7bbab1e16834a573b99a83d044d7cc34d
Tiago Cunha | ROIT <tiago.cunha@roit.com.br>
2021-06-17 21:01:51 -0300

    feat(default): add default with ref

--- test/examples/calc/impls/MultiplicationCalculator.ts
+++ test/examples/calc/impls/MultiplicationCalculator.ts
@@ -3,8 +3,8 @@ import { Calculator } from "../Calculator";
 
 @Implementation({
     key: 'multiplication*',
-    truthCustom: (key: string, metadata: any): boolean => { 
-        return key.includes('*') && metadata?.value == 10
+    truthCustom: (key: string, options: any): boolean => { 
+        return key.includes('*') && options.metadata?.value == 10
     }
 })
 export class MultiplicationCalculator implements Calculator {
